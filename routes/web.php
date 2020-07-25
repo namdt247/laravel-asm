f9d1453a54d8a0644fdd7f994a1a44d96b286a49
namdinh247 <hanoimatbao@gmail.com>
2020-07-25 20:45:29 +0700

    chỉnh sửa html admin

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.home');
 });
-Route::get('/home', function () {
-    return view('welcome');
-});
+Route::get('/home/admin', 'Frontend\HomeAdminController@home_admin');
