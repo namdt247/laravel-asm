569c7b32e84694f1703cae579ca023e2247dc3c3
namdinh247 <hanoimatbao@gmail.com>
2020-07-25 17:14:10 +0700

    them file

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('frontend.home');
+    return view('welcome');
+});
+Route::get('/home', function () {
+    return view('welcome');
 });
