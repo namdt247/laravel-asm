8bf551d7dc44b2cd93d96179f4f8db2a485fefa4
noobb-tech <anhvtth1907004@fpt.edu.vn>
2020-08-08 22:48:50 +0700

    update category2

---
+++ resources/views/frontend/categories.blade.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: namdt
+ * Date: 8/3/20
+ */
+?>
+
+@extends('frontend.layout_master')
+
+@section('main-content')
+    <div class="page-main" style="text-align: center">
+        <div class="page-title-wrapper">
+            <h1 class="page-title" id="page-title-heading" aria-labelledby="page-title-heading toolbar-amount">
+                <span class="base" data-ui-id="page-title-wrapper">Bàn</span></h1>
+        </div>
+    </div>
+    <div class="page-main"
+         style="box-sizing: border-box; margin-left: auto; margin-right: auto; width: 1072px; padding-left: 20px; padding-right: 20px;">
+        <div class="columns" style="display: block; padding-top: 20px; position: relative;">
+            <div class="columns main" style="float: right; text-align: center; width: 73%">
+                <div class="products wrapper grid products-grid" style="margin: 50px 0 10px">
+                    <ol class="products list items product-items">
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                        <li class="item product product-item"
+                            style="padding: 9px; width: calc(100%/3); text-align: center; float: left; margin:10px 0 20px; list-style-type: none">
+                            <div class="product-item-info" data-container="product-grid"
+                                 style="position: relative;">
+
+                            </div>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+            <div class="sidebar sidebar-main"
+                 style="width: 23.3%; padding-right: 0; font-family: 'Futura','Helvetica Neue',Helvetica,Arial,sans-serif; font-style: normal; font-weight: 400; line-height: 1.42857143; letter-spacing: .5px; ">
+                <div class="sidebar sidebar-main">
+                    <div class="block filter">
+                        <div class="block-content filter-content">
+                            <dl class="filter-options" id="narrow-by-list">
+                                <dt role="heading" aria-level="3" class="filter-options-title category-filter-title"
+                                    style=" font-size: 13px; letter-spacing: 1.6px; line-height: 17px; text-transform: uppercase; font-weight: 600; margin: 0 0 20px; padding-bottom: 12px;">
+                                    Danh mục sản phẩm
+                                </dt>
+                                <hr width="100%" align="center" color="#ccfff1"/>
+                                <dd class="filter-options-content">
+                                    <ul class="list-categories">
+                                        <li class="item">
+                                            <span class="open-sub-categories opened"></span>
+                                            <ul class="list-sub-categories" style="color: black">
+                                                <li class="active"><a href="#" style="color: black">Bàn</a></li>
+                                                <li><a href="#" style="color: black">Ghế</a>
+                                                </li>
+                                                <li><a href="#" style="color: black">Giường</a></li>
+                                                <li><a href="#" style="color: black">Kệ</a></li>
+                                                <li><a href="#" style="color: black">Gương</a></li>
+                                                <li><a href="#" style="color: black">Tủ</a></li>
+                                                <li><a href="#" style="color: black">Giá</a></li>
+                                                <li><a href="#" style="color: black">Bộ bàn ăn</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
