a93d5af9b3c214021bc71d7b921a44c473b6dfd2
nguyenvanhientnt <40781287+nguyenvanhientnt@users.noreply.github.com>
2020-08-10 15:33:06 +0700

    Update main_sidebar.blade.php
    aside tăng margin padding

--- resources/views/admin/include/main_sidebar.blade.php
+++ resources/views/admin/include/main_sidebar.blade.php
@@ -11,7 +11,7 @@
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column nav-compact text-sm nav-legacy" data-widget="treeview" role="menu" data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column nav-compact nav-legacy" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item has-treeview menu-open">
                     <a href="#" class="nav-link active">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
